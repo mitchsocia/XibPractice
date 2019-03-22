8b641089b15442a3286773cd297fd7cd97118b1b
Mitchell Socia <mitch.socia@detroitlabs.com>
2019-03-22 10:54:52 -0400

    Initial Commit

---
+++ XibPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XibPractice
+//
+//  Created by Mitchell Socia on 3/22/19.
+//  Copyright © 2019 Mitchell Socia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
